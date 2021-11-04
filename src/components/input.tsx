1f53c9746933e3b6860a2bf7c2128e1ae7b3f8ef
ImChangGyu <66630940+ImChangGyu@users.noreply.github.com>
2021-11-04 11:43:41 +0900

    [Add] emotion 설치

---
+++ src/components/input.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+/** @jsxImportSource @emotion/react */
+import {css, jsx} from "@emotion/react"
+
+const Input = css`
+    display: flex;
+`;
+
+const input = () => {
+    return (
+        <div css={Input}></div>
+    )
+}
+
+export default input
